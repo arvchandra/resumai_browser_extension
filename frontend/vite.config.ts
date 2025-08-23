65cc9a321621939bc6e381a5d9af4fa8263c8c0e
Arvind Chandra <arvchandra@gmail.com>
2025-08-23 15:59:30 +0530

    Revert main to skeleton React Vite project

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,24 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        popup: resolve(__dirname, "src/popup.html"),          // extension popup
-        background: resolve(__dirname, "src/background.ts"),  // background worker
-      },
-      output: {
-        entryFileNames: "assets/[name].js",
-        chunkFileNames: "assets/[name].js",
-        assetFileNames: "assets/[name].[ext]"
-      }
-
-    },
-    outDir: "dist",
-    emptyOutDir: true
-  },
 })
